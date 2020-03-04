e159c283ac65a19b3e470e1432e7721ad7fa589b
Jorge Quintero <vinicioquinterobuss@gmail.com>
2020-03-04 03:03:10 +0000

    Prevent the kill program to stop the parent of all processes and create a kernel panic

--- xv6-public/kill.c
+++ xv6-public/kill.c
@@ -11,7 +11,14 @@ main(int argc, char **argv)
     printf(2, "usage: kill pid...\n");
     exit();
   }
-  for(i=1; i<argc; i++)
-    kill(atoi(argv[i]));
+  for(i=1; i<argc; i++) {
+      int pid = atoi(argv[i]);
+      if (pid == 1) {
+          printf(1, "init cannot be killed, stop.\n");
+      } else {
+          kill(pid);
+      }
+      
+  }
   exit();
 }
