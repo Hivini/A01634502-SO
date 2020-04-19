c3bb6b625ee85c1495d1ad1d90b0fe9caf76ca1a
Jorge Quintero <vinicioquinterobuss@gmail.com>
2020-04-19 15:33:54 +0000

    Practice 8

--- practice8/chrome.c
+++ practice8/chrome.c
@@ -13,10 +13,10 @@ int main() {
         childs[i] = pid;
         if (pid != 0) {
             shmId = shmget(400, 100, 0644);
-            char *var = (char *)shmat(shmId, NULL, 0);
-            var = &var[i * 10];
+            int *nums = (int *)shmat(shmId, NULL, 0);
+            nums = &nums[i * 10];
             for (int j = 0; j < 10; j++) {
-                var[j] = i + '0';
+                nums[j] = i + '0';
             }
             return 0;
         }
@@ -24,7 +24,7 @@ int main() {
     for (int i = 0; i < 5; i++) {
         waitpid(childs[i], 0, 0);
     }
-    char *var = (char *) shmat(shmId, NULL, 0);
-    printf("Contenido de var %s\n", var);
+    int *nums = (int *) shmat(shmId, NULL, 0);
+    printf("Contenido de nums %d\n", *nums);
     return 0;
 }
